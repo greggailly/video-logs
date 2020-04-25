37ac42450f2f70cff6dc03d3e9bfd97e82c5559f
greggailly <greggailly@yahoo.fr>
2020-04-25 14:36:48 +0200

    added auth

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
      */
     public function index(LoggerInterface $dbLogger)
     {
+        dd($this->getUser());
         $dbLogger->info('Notre premier log');
 
         return $this->render('home/index.html.twig', [
